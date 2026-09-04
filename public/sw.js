45974f0fab714f4c4fee589bf7a4645728df1138
SurviveLabs <officialsurvivelabs@gmail.com>
2026-09-04 21:21:25 +0100

    Update service worker to version 2 and improve caching

--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,15 @@
-const CACHE_NAME = 'matchday-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = 'matchday-v2';
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
-
