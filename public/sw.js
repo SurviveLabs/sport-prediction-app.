1c4d9a427c6bace3780f955bf2af741ec1b8d83f
SurviveLabs <officialsurvivelabs@gmail.com>
2026-09-05 03:57:19 +0100

    Fix clients.claim() reference in service worker
    Updated clients.claim() to use the correct reference.

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,13 @@
+// Minimal Service Worker to satisfy PWA installation criteria
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
+  // Pass all network requests through directly to preserve statelessness
   event.respondWith(fetch(event.request));
 });
